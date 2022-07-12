e7fe96ffb263f3e0cf807c73b06e477239ceda18
Herbert <hebert.aggrandize@fornecedores.klabin.com.br>
2022-07-12 14:05:31 -0300

    8. Anadiendo la clase Entity Alumno

---
+++ microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/models/entity/Alumno.java
@@ -0,0 +1,42 @@
+package com.formacionbdi.microservicios.app.usuarios.models.entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "alumnos")
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Alumno implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = new Date();
+    }
+}
