dd42562cfa79dde70182ec333704e6f7b7eeb6ad
Herbert <hebert.aggrandize@fornecedores.klabin.com.br>
2022-07-12 15:28:34 -0300

    16. Creando servidor de registro Eureka.

--- microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/MicroserviciosUsuariosApplication.java
+++ microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/MicroserviciosUsuariosApplication.java
@@ -2,8 +2,10 @@ package com.formacionbdi.microservicios.app.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviciosUsuariosApplication {
 
 	public static void main(String[] args) {
